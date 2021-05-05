c886a6c279c531be1b10c7ed8a0f4dafe9f0171a
palel <32599663+palel@users.noreply.github.com>
2021-05-05 22:25:00 +0200

    remove tasks

--- js/script.js
+++ js/script.js
@@ -10,20 +10,6 @@
         },
     ];
 
-    const render = () => {
-        let htmlString = "";
-
-        for (const task of tasks) {
-            htmlString += `
-                <li${task.done ? " class=\"section__listItem--done\"" : ""}>
-                     ${task.content}
-                </li>
-            `;
-        }
-
-        document.querySelector(".js-tasks").innerHTML = htmlString;
-    };
-
     const addNewTask = (newTaskContent) => {
         tasks.push({
             content: newTaskContent,
@@ -31,22 +17,54 @@
         render();
     };
 
+    const removeTask = (taskIndex) => {
+        tasks.splice(taskIndex, 1);
+        render();
+    };
 
+    const render = () => {
+        let htmlString = "";
+
+        for (const task of tasks) {
+            htmlString += `
+                <li class=\"unorderderList__listItem ${task.done ? " unorderderList__listItem--done\"" : "\""}>
+                    <button class=\"unorderderList__button\">${task.done ? "✅" : "☑️"}</button> 
+                    ${task.content}
+                     <button class=\"unorderderList__button js-remove\">❌</button>
+                </li>
+            `;
+        }
+
+        document.querySelector(".js-tasks").innerHTML = htmlString;
+
+        const removeButtons = document.querySelectorAll(".js-remove");
+
+        removeButtons.forEach((removeButton, index) => {
+            removeButton.addEventListener("click", () => {
+                removeTask(index);
+            });
+        });
+    };
+
+    
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const newTaskContent = document.querySelector(".js-newTask").value.trim();
+        if (newTaskContent === "") {
+            return;
+        }
+
+        addNewTask(newTaskContent);
+
+    };
 
     const init = () => {
         render();
 
         const form = document.querySelector(".js-form");
-        form.addEventListener("submit", (event) => {
-            event.preventDefault();
-
-            const newTaskContent = document.querySelector(".js-newTask").value.trim();
-            if(newTaskContent === ""){
-                return;
-            }
-
-            addNewTask(newTaskContent);            
-        }); 
+        form.addEventListener("submit", onFormSubmit);
     };
     init();
 }
